3d1ce2b1991c9855777b6cbb913ee339f41a7b0b
ILYAS <tabennhas.ilyas@univ-bechar.dz>
2024-05-04 12:48:45 +0100

    Trees data structure and BSTs

---
+++ print_tree.py
@@ -0,0 +1,10 @@
+
+def print_tree(tree, level=0, prefix="Root:"):
+    if tree is not None:
+        print(" " * (level * 4) + prefix + str(tree.data))
+        if tree.left is not None or tree.right is not None:
+            print_tree(tree.left, level + 1, "L--")
+            print_tree(tree.right, level + 1, "R--")
+
+# Example usage:
+print_tree(new_tree)
